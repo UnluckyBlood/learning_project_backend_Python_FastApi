1b445d9310aa1b16964c6ae1ad2d2143b53ce5bf
Nguruvilu <unluckyblood@mail.ru>
2026-03-13 23:05:43 +0300

    Add_config

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+load_dotenv
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings #Создаём экземпляр класса, не требует снова перечитывать а прост передаём гот переменную
