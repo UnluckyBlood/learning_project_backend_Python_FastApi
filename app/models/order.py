6b70f2cab78bd1a2194098a8646faf5c150afdc5
Nguruvilu <unluckyblood@mail.ru>
2026-03-14 17:05:17 +0300

    Model_ZAKAZOOOB

---
+++ app/models/order.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, func
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+#класс таблицы юзеров, имени, айдишки и пароля и когда зареган
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+#класс таблицы заказов, кто заказал что сколько когда по какой цене и какой статус
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    customer_name = Column(String(100), nullable=False)
+    product_name = Column(String(200), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    total_price = Column(Numeric(10,2),nullable=False)
+    status = Column(String(20), nullable=False, default="На рассмотрении")
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
