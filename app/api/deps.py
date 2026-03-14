c979263fbdfc0e8ca75d7513576d33f45a760656
Nguruvilu <unluckyblood@mail.ru>
2026-03-14 20:57:26 +0300

    auth_check_token_in head

---
+++ app/api/deps.py
@@ -0,0 +1,36 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+from app.core.config import settings
+from app.db.session import get_db
+from app.models.order import User
+# проверка отефикации из токена
+oauth2_scheme = OAuth2PasswordBearer(rokenUrl="/auth/login") #Ожидание токена в заголовке
+
+#декодировка токена
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Не удалось подтвердить учетные данные",
+        headers={"WWW-Аутентификация":"Носитель"},
+    )
+    try:
+        payload = jwt.decode(
+            token, 
+            settings.JWT_SECRET, 
+            algorithms=[settings.JWT_ALGORITHM]
+        )
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(User).filter(User.username == username).first()
+    if user is None:
+        raise credentials_exception
+    return user
